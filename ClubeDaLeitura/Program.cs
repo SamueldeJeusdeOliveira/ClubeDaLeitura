0ed040e3c8de6f088e931dee553a7955c798d4b4
SamueldeJeusdeOliveira <samuksjj@gmail.com>
2025-05-27 21:38:51 -0300

    Implementação da Program

--- ClubeDaLeitura/Program.cs
+++ ClubeDaLeitura/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace ClubeDaLeitura;
+﻿using ClubeDaLeitura.Compartilhado;
+
+namespace ClubeDaLeitura;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
 
+        while (true)
+        {
+            telaPrincipal.ApresentarMenuPrincipal();
+
+            TelaBase telaEscolhida = telaPrincipal.ObterTela();
+
+            if (telaEscolhida == null)
+                break;
+
+            char opcaoEscolhida = telaEscolhida.ApresentarMenu();
+
+            if (opcaoEscolhida == 'S')
+                break;
+
+            switch (opcaoEscolhida)
+            {
+                case '1':
+                    telaEscolhida.CadastrarRegistro();
+                    break;
+
+                case '2':
+                    telaEscolhida.VisualizarRegistros(true);
+                    break;
+
+                case '3':
+                    telaEscolhida.EditarRegistro();
+                    break;
+
+                case '4':
+                    telaEscolhida.ExcluirRegistro();
+                    break;
+            }
+        }
     }
 }
