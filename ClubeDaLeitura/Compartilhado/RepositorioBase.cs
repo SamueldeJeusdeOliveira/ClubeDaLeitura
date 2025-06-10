70b89b87d48ce6d31994a05f8325cdbb21a3815f
SamueldeJeusdeOliveira <samuksjj@gmail.com>
2025-06-10 18:21:41 -0300

    Reimplementacao do RepositorioBase

--- ClubeDaLeitura/Compartilhado/RepositorioBase.cs
+++ ClubeDaLeitura/Compartilhado/RepositorioBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,65 +9,33 @@ namespace ClubeDaLeitura.Compartilhado
 {
     public abstract class RepositorioBase
     {
-        private EntidadeBase[] registros = new EntidadeBase[100];
-        private int contadorRegistros = 1;
+        protected ArrayList registros = new ArrayList();
 
-        public void CadastrarRegistro(EntidadeBase novoRegistro)
+        public virtual void CadastrarRegistro(object registro)
         {
-            registros[contadorRegistros] = novoRegistro;
-
-            contadorRegistros++;
+            registros.Add(registro);
         }
 
-        public bool EditarRegistro(int idSelecionado, EntidadeBase registroAtualizado)
-        {
-            EntidadeBase registroSelecionado = SelecionarRegistroPorId(idSelecionado);
-
-            if (registroSelecionado == null)
-                return false;
-
-            registroSelecionado.AtualizarRegistro(registroAtualizado);
-
-            return true;
-        }
-
-        public bool ExcluirRegistro(int idSelecionado)
-        {
-            for (int i = 0; i < registros.Length; i++)
-            {
-                if (registros[i] == null)
-                    continue;
-
-                else if (registros[i].id == idSelecionado)
-                {
-                    registros[i] = null;
-
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public EntidadeBase[] SelecionarRegistros()
+        public virtual ArrayList SelecionarTodos()
         {
             return registros;
         }
 
-        public EntidadeBase SelecionarRegistroPorId(int idSelecionado)
+        public virtual object SelecionarRegistroPorId(int id)
         {
-            for (int i = 0; i < registros.Length; i++)
+            foreach (var item in registros)
             {
-                EntidadeBase registro = registros[i];
-
-                if (registro == null)
-                    continue;
-
-                if (registro.id == idSelecionado)
-                    return registro;
+                if (((dynamic)item).id == id)
+                    return item;
             }
-
             return null;
+        }
+
+        public virtual void ExcluirRegistro(int id)
+        {
+            object registro = SelecionarRegistroPorId(id);
+            if (registro != null)
+                registros.Remove(registro);
         }
     }
 }
